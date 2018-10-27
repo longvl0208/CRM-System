52cb0ea3167d67dad5a3e5e75aaadce8e495065b
longvl0208 <longlv0208@gmail.com>
2018-10-28 01:09:30 +0700

    first commit

---
+++ src/main/java/com/longvl/springdemo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.longvl.springdemo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
